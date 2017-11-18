e298fc426736872d117a4050ec210e74a9554117
Igor Liferenko <igor.liferenko@gmail.com>
2017-11-18 17:02:54 +0700

    edit

--- build-printserver.sh
+++ build-printserver.sh
@@ -9,12 +9,12 @@ if [ `whereami` != home ]; then
   exit
 fi
 
-IMG=lede-imagebuilder-17.01.4-ar71xx-generic.Linux-x86_64
-SDK=lede-sdk-17.01.4-ar71xx-generic_gcc-5.4.0_musl-1.1.16.Linux-x86_64
+IMG=lede-imagebuilder-17.01.4-ramips-rt305x.Linux-x86_64
+SDK=lede-sdk-17.01.4-ramips-rt305x_gcc-5.4.0_musl-1.1.16.Linux-x86_64
 mkdir -p ~/lede
 cd ~/lede
-[ -e $IMG.tar.xz ] || wget https://downloads.lede-project.org/releases/17.01.4/targets/ar71xx/generic/$IMG.tar.xz || exit
-[ -e $SDK.tar.xz ] || wget https://downloads.lede-project.org/releases/17.01.4/targets/ar71xx/generic/$SDK.tar.xz || exit
+[ -e $IMG.tar.xz ] || wget https://downloads.lede-project.org/releases/17.01.4/targets/ramips/rt305x/$IMG.tar.xz || exit
+[ -e $SDK.tar.xz ] || wget https://downloads.lede-project.org/releases/17.01.4/targets/ramips/rt305x/$SDK.tar.xz || exit
 rm -fr printserver/
 mkdir printserver/
 cd printserver/
@@ -31,7 +31,7 @@ uci set wireless.default_radio0.key=mirumirmirumirmir
 uci commit wireless
 uci del network.lan.ifname
 uci del network.lan.type
-uci set network.lan.ipaddr=192.168.1.9
+uci set network.lan.ipaddr=192.168.1.3
 uci set network.lan.gateway=192.168.1.1
 uci set network.lan.dns=192.168.1.1
 uci commit network
@@ -41,14 +41,14 @@ uci set system.@system[0].timezone=GMT-7
 uci commit system
 EOF
 mkdir -p files/bin/
-ln -s /mnt/tel files/bin/tel
+ln -s /mnt/prt files/bin/prt
 mkdir -p files/etc/
 cat <<'EOF' >files/etc/rc.local
 cat <<'FOE' | sh &
 while ! mount|grep -q ^192.168.1.8; do
   mount -t nfs -o nolock 192.168.1.8:/mnt/ /mnt/
 done
-tel &
+prt
 FOE
 exit 0
 EOF
@@ -57,8 +57,8 @@ cat <<'EOF' >files/bin/pkill
 kill `pgrep "$@"`
 EOF
 chmod +x files/bin/pkill
-make image PROFILE=tl-wr842n-v3 PACKAGES="mpc kmod-fs-nfs nfs-utils" FILES=files/ # netcat strace kmod-usb-printer # NOTE: kmod-usb2 may be required if you switch to WT1520
+make image PROFILE=wt1520-8M PACKAGES="lsof netcat strace kmod-usb-printer kmod-fs-nfs nfs-utils" FILES=files/ # NOTE: if printer will not work, install kmod-usb2 (see via git lg if it was used for 1043nd and if not - mark here that it is required specifically for this device)
 rm -f /usr/local/SUPER_DEBIAN/printserver-sdk.tar.xz
 cp ../../$SDK.tar.xz /usr/local/SUPER_DEBIAN/printserver-sdk.tar.xz
 rm -f /usr/local/SUPER_DEBIAN/printserver-sysupgrade.img
-cp bin/targets/ar71xx/generic/lede-17.01.4-ar71xx-generic-tl-wr842n-v3-squashfs-factory.bin /usr/local/SUPER_DEBIAN/printserver-factory.img
+cp bin/targets/ramips/rt305x/lede-17.01.4-ramips-rt305x-wt1520-8M-squashfs-sysupgrade.bin /usr/local/SUPER_DEBIAN/printserver-sysupgrade.img
