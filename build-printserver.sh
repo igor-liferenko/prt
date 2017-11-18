7570cac08b04cce8e2d000d6168d96cac40ca748
Igor Liferenko <igor.liferenko@gmail.com>
2017-11-18 09:51:25 +0700

    use new router for tel.w

--- build-printserver.sh
+++ build-printserver.sh
@@ -9,12 +9,12 @@ if [ `whereami` != home ]; then
   exit
 fi
 
-IMG=lede-imagebuilder-17.01.4-ramips-rt305x.Linux-x86_64
-SDK=lede-sdk-17.01.4-ramips-rt305x_gcc-5.4.0_musl-1.1.16.Linux-x86_64
+IMG=lede-imagebuilder-17.01.4-ar71xx-generic.Linux-x86_64
+SDK=lede-sdk-17.01.4-ar71xx-generic_gcc-5.4.0_musl-1.1.16.Linux-x86_64
 mkdir -p ~/lede
 cd ~/lede
-[ -e $IMG.tar.xz ] || wget https://downloads.lede-project.org/releases/17.01.4/targets/ramips/rt305x/$IMG.tar.xz || exit
-[ -e $SDK.tar.xz ] || wget https://downloads.lede-project.org/releases/17.01.4/targets/ramips/rt305x/$SDK.tar.xz || exit
+[ -e $IMG.tar.xz ] || wget https://downloads.lede-project.org/releases/17.01.4/targets/ar71xx/generic/$IMG.tar.xz || exit
+[ -e $SDK.tar.xz ] || wget https://downloads.lede-project.org/releases/17.01.4/targets/ar71xx/generic/$SDK.tar.xz || exit
 rm -fr printserver/
 mkdir printserver/
 cd printserver/
@@ -31,7 +31,7 @@ uci set wireless.default_radio0.key=mirumirmirumirmir
 uci commit wireless
 uci del network.lan.ifname
 uci del network.lan.type
-uci set network.lan.ipaddr=192.168.1.3
+uci set network.lan.ipaddr=192.168.1.9
 uci set network.lan.gateway=192.168.1.1
 uci set network.lan.dns=192.168.1.1
 uci commit network
@@ -40,13 +40,24 @@ uci commit dhcp
 uci set system.@system[0].timezone=GMT-7
 uci commit system
 EOF
+mkdir -p files/bin/
+ln -s /mnt/tel files/bin/tel
 mkdir -p files/etc/
-cat << EOF > files/etc/rc.local
+cat <<'EOF' >files/etc/rc.local
+cat <<'FOE' | sh &
+while ! mount|grep -q ^192.168.1.8; do
+  mount -t nfs -o nolock 192.168.1.8:/mnt/ /mnt/
+done
 tel &
-prt
+FOE
 exit 0
 EOF
-make image PROFILE=wt1520-8M PACKAGES="mpc netcat kmod-usb-printer strace procps-ng-pkill" FILES=files/ # NOTE: kmod-usb2 may be required if you switch to WT1520
+cat <<'EOF' >files/bin/pkill
+#!/bin/sh
+kill `pgrep "$@"`
+EOF
+chmod +x files/bin/pkill
+make image PROFILE=tl-wr842n-v3 PACKAGES="mpc kmod-fs-nfs nfs-utils" FILES=files/ # netcat strace kmod-usb-printer # NOTE: kmod-usb2 may be required if you switch to WT1520
 rm -f /usr/local/SUPER_DEBIAN/printserver-sdk.tar.xz
 cp ../../$SDK.tar.xz /usr/local/SUPER_DEBIAN/printserver-sdk.tar.xz
 rm -f /usr/local/SUPER_DEBIAN/printserver-sysupgrade.img
