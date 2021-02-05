485555f8ce39b99cd0acd43c2d8c72b997df7292
Igor Liferenko <igor.liferenko@gmail.com>
2021-02-05 10:06:53 +0700

    edit

--- dvipdfmx-select.pl
+++ dvipdfmx-select.pl
@@ -1,11 +1,10 @@
-#!/bin/perl -lw
-# comma-separated list of pages is given in the first line of input;
-# indexes to this list (1-based) are given in the second line of input;
+#!/bin/perl -lwn
+# comma-separated list of pages is given in input;
+# indexes to this list (1-based) are given in argument;
 # output [comma-separated list of] pages which have given indexes
-$_=<>; chomp;
-@p=split/,/;
-$_=<>; chomp;
-for (split/,/) {
+BEGIN { $a = shift }
+@p = split /,/;
+for (split /,/, $a) {
   if (/(\d+)-(\d+)/) {
     for ($i = $1; $i <= $2; $i++) {
       push @idx, $i;
