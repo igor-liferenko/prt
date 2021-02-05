c23251ca8e5f36d5c825787668caf12fe495e860
Igor Liferenko <igor.liferenko@gmail.com>
2021-02-05 14:34:51 +0700

    edit

--- dvipdfmx-select.pl
+++ dvipdfmx-select.pl
@@ -1,7 +1,7 @@
 #!/bin/perl -lwn
 # comma-separated list of pages is given in input;
 # indexes to this list (1-based) are given in argument;
-# output [comma-separated list of] pages which have given indexes
+# output comma-separated list of pages which have given indexes
 BEGIN { $a = shift }
 @p = split /,/;
 for (split /,/, $a) {
