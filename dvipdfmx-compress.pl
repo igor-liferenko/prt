c03b64d8f71502e00fe609d77afabb615ef0727b
Igor Liferenko <igor.liferenko@gmail.com>
2021-02-05 09:20:22 +0700

    edit

--- dvipdfmx-compress.pl
+++ dvipdfmx-compress.pl
@@ -1,2 +1,11 @@
-#!/bin/perl -wl
+#!/bin/perl -wln
 # translate comma-separated list of pages to shorter form by using ranges
+for (split/,/) {
+  $first = $_, continue if !defined $first;
+  if ($_ == $first+1) {
+    $last=$_;
+  }
+  else ($first {
+    print 
+  }
+}
--- dvipdfmx-select.pl
+++ dvipdfmx-select.pl
@@ -1,4 +1,7 @@
 #!/bin/perl -lw
+# comma-separated list of pages is given in the first line of input;
+# indexes to this list (1-based) are given in the second line of input;
+# output [comma-separated list of] pages which have given indexes
 $_=<>; chomp;
 @p=split/,/;
 $_=<>; chomp;
