eefc99da33a7620ce0110b03a91f1a3a0eb958ae
Igor Liferenko <igor.liferenko@gmail.com>
2021-02-05 13:39:40 +0700

    edit

--- dvipdfmx-compress.pl
+++ dvipdfmx-compress.pl
@@ -1,11 +1,15 @@
 #!/bin/perl -wln
-# translate comma-separated list of pages to shorter form by using ranges
-for (split/,/) {
-  $first = $_, continue if !defined $first;
-  if ($_ == $first+1) {
-    $last=$_;
+# make comma-separated list of pages shorter by using ranges
+for ( ( split /,/ ), 0 ) { # (add one extra element)
+  unless (defined $l) { # Only on the first line.
+    $l = $p = $_;
+    next;
   }
-  else ($first {
-    print 
-  }
+  # If present line ($_) follows previous line ($p), continue.
+  if ( $_ == $p+1 ) { $p = $_; next }
+  # Starting a new range ($_>$p+1): print the previous range.
+  push @r, ( $l==$p ? $l : $l . '-' . $p );
+  # Save values in the variables left ($l) and previous ($p).
+  $l = $p = $_;
 }
+print join ',', @r;
