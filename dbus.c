1f1633d981470ce4e5749b4fed6f9b99ad896888
Igor Liferenko <igor.liferenko@gmail.com>
2021-02-07 18:24:38 +0700

    edit

--- dbus.c
+++ dbus.c
@@ -1,6 +1,8 @@
-///usr/lib/cups/notifier/dbus
+/* Compile this and replace /usr/lib/cups/notifier/dbus */
 int
 main(void)
 {
   return (1);
 }
+/* TODO: after checking that this works for disabling the popup, try
+   to link this to /bin/false instead */
