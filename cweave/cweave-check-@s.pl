cf7ee12cf9346709b54f0f870d070bee2ae75e9c
Igor Liferenko <igor.liferenko@gmail.com>
2021-02-25 12:16:55 +0700

    move

--- cweave/cweave-check-@s.pl
+++
@@ -1,9 +0,0 @@
-#!/bin/perl -lwn
-BEGIN { $idx = pop }
-if (/\@s ([^ ]+)/) {
-  $x = $y = $1;
-  $x =~ s/_/\\\\_/g;
-  if (system("grep -q '{$x}' $idx") != 0) {
-    print STDERR "WARNING: remove \@s $y ...";
-  }
-}
