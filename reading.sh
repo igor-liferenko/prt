5defd10fd8e0898ac99fbec0b18979fe2fb8254a
Igor Liferenko <igor.liferenko@gmail.com>
2024-04-29 17:34:38 +0700

    edit

--- reading.sh
+++ reading.sh
@@ -1,3 +1,4 @@
+grep -q 'begin reading\.tex' $1 && { echo ERROR: second run; exit 1; }
 iconv -f UTF-8 -t UCS-2 $1 >/dev/null 2>&1 || { echo File is not UTF-8; exit 1; }
 sed -i 's/\r$//' $1
 sed -i 's/$/\n/' $1
@@ -8,3 +9,4 @@ sed -i 's/–/--/g' $1 # EN DASH
 sed -i 's/—/---/g' $1 # EM DASH
 # sed -i 's/\xCC\x81//g' $1 # COMBINING ACUTE ACCENT
 { rm $1; ( echo % begin reading.tex; cat ~/prt/reading.tex; echo % end reading.tex; cat ) >$1; } <$1
+echo '\bye' >>$1
